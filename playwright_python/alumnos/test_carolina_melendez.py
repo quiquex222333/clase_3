a61329899b572382e3f163843b0ec250be899113
unknown <carolina.melendez376@gmail.com>
2025-09-09 21:54:26 -0400

    Added test youtube search component  visible

--- playwright_python/alumnos/test_carolina_melendez.py
+++ playwright_python/alumnos/test_carolina_melendez.py
@@ -6,3 +6,10 @@ def test_youtube_logo_visible(page: Page):
     # Verificar que el logo de youtube sea visible
     expect(page.locator("#logo").first).to_be_visible()
 
+def test_youtube_search_bar_visible(page: Page):
+    page.goto("https://www.youtube.com/")
+
+    # Verificar que la barra de búsqueda sea visibles
+    page.locator("input[name='search_query']")
+
+
