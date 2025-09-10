8a5868b97132de07e711af41003bf00f51d037c6
Sergio <201801656@est.umss.edu>
2025-09-09 22:07:12 -0400

    Tarea

--- playwright_python/alumnos/test_sergio_soliz.py
+++ playwright_python/alumnos/test_sergio_soliz.py
@@ -5,19 +5,20 @@ def test_has_title(page: Page):
     page.goto("https://www.facebook.com/")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Facebook - log in or sign up"))
+    expect(page).to_have_title(re.compile("Facebook - Entrar o registrarse"))
 
 def test_get_started_link(page: Page):
+
     page.goto("https://www.youtube.com/")
 
+    # Esperar a que aparezca el input de búsqueda
+    page.wait_for_selector("input[placeholder='Buscar']")
+
+    # Llenar la búsqueda
+    page.locator("input[placeholder='Buscar']").fill("Playwright Python")
+
     # Click en el botón de búsqueda
-    page.get_by_role("button", name="Buscar").click()
+    page.locator("button[title='Buscar']").click()
 
-    # Escribir algo en la barra de búsqueda
-    page.get_by_role("combobox", name="Buscar").fill("Playwright Python")
-
-    # Presionar Enter
-    page.keyboard.press("Enter")
-
-    # Validar que aparezcan resultados (por ejemplo que exista un enlace a un video)
-    expect(page.get_by_role("link").first).to_be_visible()
+    # Validar que aparezcan resultados
+    expect(page.locator("ytd-video-renderer").first).to_be_visible()
